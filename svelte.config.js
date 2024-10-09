dbca532ac43a1d393ea1f1c0f1c79b27b548c33c
wak4kusa296 <162152488+wak4kusa296@users.noreply.github.com>
2024-10-09 09:55:03 +0900

    Hello

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+	// SvelteKitで使用されるプリプロセッサを設定
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// Renderなどでのデプロイに対応するため、Node.jsアダプターを使用
+		adapter: adapter({
+			out: 'build', // 出力先を 'build' フォルダに指定
+			precompress: false, // 必要に応じて、gzip圧縮などのプリコンプレスを有効にできます
+			env: {
+				port: process.env.PORT || 3000 // デフォルトでポート3000を使用
+			}
+		})
 	}
 };
 
